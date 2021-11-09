979189fdab30c37d30437251ef5731f980b56418
Alex Varakin <avarakin@gmail.com>
2021-11-09 18:30:19 -0500

    first commit

---
+++ MirroGenerator.py
@@ -0,0 +1,30 @@
+#this program generates code for grinding a parabolic mirror
+#Initial formula used is y^2 = 4ax
+#It can be converter to x^2 = 4az or x = sqrt (4az)
+
+import math
+
+
+focal_length = 600
+diamiter = 150
+depth_of_cut = 0.1
+radius_increment = 2
+sagitta = (diamiter/2)*(diamiter/2) / (4 * focal_length)
+
+#hogging steps - for each depth of cut we have to run concentric circles to remove the material
+z = 0
+while True:
+    radius = math.sqrt (4*focal_length*z)
+    if radius > diamiter/2:
+        break
+    x = radius
+    print(  "z=%.3f" % z, "x=%.3f" % x)
+    while True:
+        x -= radius_increment
+        if x < 0 :
+            break
+    z += depth_of_cut
+
+print(  "sagitta=%.3f" % sagitta)
+
+#fine grinding steps - we need to shape the form, no concentric circles needed here, because all the material was removed as part of hogging steps
