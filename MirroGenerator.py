948a081fd3fa679a36161f841ccd36b118434093
Alex Varakin <avarakin@gmail.com>
2021-11-09 19:18:35 -0500

    Changed strategy to be constant radius decrement as opposed to constant depth of cut

--- MirroGenerator.py
+++ MirroGenerator.py
@@ -7,24 +7,26 @@ import math
 
 focal_length = 600
 diamiter = 150
-depth_of_cut = 0.1
-radius_increment = 2
+radius_increment = 3
 sagitta = (diamiter/2)*(diamiter/2) / (4 * focal_length)
 
 #hogging steps - for each depth of cut we have to run concentric circles to remove the material
 z = 0
+base_radius = diamiter / 2
+
 while True:
-    radius = math.sqrt (4*focal_length* (sagitta - z))
-    x = radius
-    print(  "z=%.3f" % z, "x=%.3f" % x)
+    base_radius -= radius_increment
+    if base_radius <= 0:
+        break
+
+    z = sagitta - base_radius*base_radius / (4 * focal_length)
+    print(  ";Processing slice z=%.3f" % z, "x=%.3f" % base_radius)
+    x = base_radius
     while True:
+        print(  "z=%.3f" % z, "x=%.3f" % x)
         x -= radius_increment
         if x < 0 :
             break
-    z += depth_of_cut
-    if z >= sagitta:
-        break
-
 print(  "sagitta=%.3f" % sagitta)
 
 #fine grinding steps - we need to shape the form, no concentric circles needed here, because all the material was removed as part of hogging steps
