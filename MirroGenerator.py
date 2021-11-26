eb71f7b3f15eacf0ad206dc15d500f5557d0c49d
Alex Varakin <avarakin@gmail.com>
2021-11-26 16:20:12 -0500

    Implemented helix strategy

--- MirroGenerator.py
+++ MirroGenerator.py
@@ -1,9 +1,9 @@
 import math
 
 focal_length = 600
-diamiter = 150
+diamiter = 600
 radius_increment = 4
-radius_increment_fine = 0.5
+radius_increment_fine = 10
 sagitta = (diamiter/2)*(diamiter/2) / (4 * focal_length)
 feed_rate = 1000
 
@@ -33,28 +33,61 @@ def hogging():
                 break
 
 
+
+#def spiral(start_radius, end_radius, depth):
+#    print( "G0 X0.000 Y0.000 Z5.000")    
+
 def finishing():
     print(  "(Fine Grinding mirror with sagitta=%.3f)" % sagitta)
-    print( "G0 X0.000 Y0.000 Z5.000")
 
-    z = 0
     base_radius = diamiter / 2
 
+    print( "G0 X-%.3f" %base_radius, " Y0.000 Z0.000")
+
+    z = 0
+
+
+
     while True:
+        base_radius_old = base_radius
         base_radius -= radius_increment_fine
         if base_radius <= 0:
             break
 
+        z_old = z
         z = -(sagitta - base_radius*base_radius / (4 * focal_length))
-        x = base_radius
-        print(  "G0 X-%.3f" %x,  " Y0 ", "Z%.3f" %z)
-        print(  "G02 X-%.3f" %x,  " Y0 ", "I%.3f" %x, " J0 F%.1f" % feed_rate)
+        z_delta = (z - z_old)*2
+        
+
+        x1 = 0 - radius_increment_fine/4
+        r1 = base_radius - radius_increment_fine/4
+        z1 = z_old + z_delta/4
+        y1 = base_radius - radius_increment_fine/4
+        print(  "G02 X%.3f" %x1,  " Y%.3f" %y1, " R%.3f" %r1, " Z%.3f" %z1, " F%.1f" % feed_rate)
+
+
+        x2 = base_radius - radius_increment_fine/2
+        y2 = 0
+        z2 = z_old + z_delta/2
+        r2 = base_radius - radius_increment_fine/2
+        print(  "G02 X%.3f" %x2,  " Y%.3f" %y2, " R%.3f" %r2, " Z%.3f" %z2, " F%.1f" % feed_rate)
+
+        x3 = 0 - radius_increment_fine * 0.75
+        y3 = -(base_radius - radius_increment_fine*0.75)
+        z3 = z_old + z_delta*0.75
+        r3 = base_radius - radius_increment_fine*0.75
+        print(  "G02 X%.3f" %x3,  " Y%.3f" %y3, " R%.3f" %r3, " Z%.3f" %z3, " F%.1f" % feed_rate)
+
+        x4 = -(base_radius - radius_increment_fine)
+        y4 = 0
+        z4 = z_old + z_delta
+        r4 = base_radius - radius_increment_fine
+        print(  "G02 X%.3f" %x4,  " Y%.3f" %y4, " R%.3f" %r4, " Z%.3f" %z4, " F%.1f" % feed_rate)
+
+
 
 print( "G17 G90 G21 G54")
 finishing()
 
-
-
-#TODO:  fine grinding steps - we need to shape the form, no concentric circles needed here, because all the material was removed as part of hogging steps
 
 
